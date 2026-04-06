1a0c82e4fce64028427628488bd2826f31cca9e7
Vitali Caballero <vitalicaballero500@gmail.com>
2026-04-06 12:29:54 +0800

    F1 Update: Describe what you changed here

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        // === THE FIX: GLOBAL THEME INJECTION ===
-        // By redefining this variable, ALL inner modules will instantly switch 
-        // from Cyan to the Official POSO Emerald Green.
-        emerald-600: '#059669', // Tailwind emerald-600
-        bg-emerald-900: '#022c22'    // Tailwind bg-emerald-900
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
